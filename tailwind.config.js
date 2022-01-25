57ee3fd2ca524b0b7ac6b90afd7399fb49ce0c09
Justice Smith <jcode94ttv@gmail.com>
2022-01-25 16:06:36 -0500

    redesign contacts.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#009579",
+                dark: "#007f67",
+                secondary: "#252c6a",
+                error: "#cc3333",
+                success: "#4bb544",
+            },
+        },
+    },
+    plugins: [],
+};
