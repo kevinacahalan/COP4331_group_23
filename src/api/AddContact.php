9854e3031641a01d3c08daeddaa746359cd88e85
Kevin Cahalan <kevinacahalan@gmail.com>
2022-01-31 00:55:52 -0500

    +Done AddContact

--- src/api/AddContact.php
+++ src/api/AddContact.php
@@ -3,7 +3,12 @@
 	
 	$Login = $inData["login"];
 	$Password = $inData["password"];
-    //  a contact struct or something = $inData["contact_data"];
+	$contact = $inData["contact"];
+
+	$firstName = $contact["firstName"];
+	$lastName = $contact["lastName"];
+	$email = $contact["email"];
+	$phoneNumber = $contact["phoneNumber"];
 
 	// enable error reporting
 	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
@@ -16,18 +21,42 @@
 	} 
 	else
 	{	
-        returnWithError("bla bla bla, not done");
-		// try {
-		// 	$stmt = $conn->prepare("INSERT into Contacts bla bla bla bla bla");
-		// 	$stmt->bind_param("ss", $Login, $Password);
-		// 	$stmt->execute();
-		// 	$stmt->close();
-		// 	$conn->close();
-			
-		// 	returnWithError(""); // Account made, no error
-		// } catch (mysqli_sql_exception $e) {
-		// 	returnWithError("login already in use");
-		// }
+		$userID = getUserID($conn, $Login, $Password);
+
+		try {
+			$stmt = $conn->prepare("INSERT into Contacts (FirstName,LastName,Email,PhoneNumber,UserID) VALUES(?,?,?,?,?)");
+			$stmt->bind_param("sssss", $firstName, $lastName, $email, $phoneNumber, $userID);
+			$stmt->execute();
+
+			returnWithError(""); // Account made, no error
+		} catch (mysqli_sql_exception $e) {
+			returnWithError("phone number already in use");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	// Grabs userID for login password combination, spits error and quits
+	// if no combination found.
+	function getUserID($conn, $login, $password)
+	{
+		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=? AND Password =?");
+		$stmt->bind_param("ss", $login, $password);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if( $row = $result->fetch_assoc() )
+		{
+			$stmt->close();
+			return $row['ID'];
+		}
+		else
+		{
+			returnWithError("No Records Found");
+			$stmt->close();
+			exit();
+		}
 	}
 
 	function getRequestInfo()
@@ -46,5 +75,9 @@
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
-?>
+
+	function returnExitAndDumpString( $dump )
+	{
+		sendResultInfoAsJson('{ "dump" : "' . $dump . '" }');
+		exit();
+	}
