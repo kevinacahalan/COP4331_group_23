b61c162a1648dc1fd0993682170860fb380ca8eb
Kevin Jimenez <Kevinjimenez0226@gmail.com>
2022-02-01 00:16:14 -0500

    Added Update.php

---
+++ src/api/Update.php
@@ -0,0 +1,37 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Email=?, PhoneNumber=? WHERE ID=?");
+		$stmt->bind_param("sssss", $inData["firstName"], $inData["lastName"], $inData["email"], $inData["phoneNumber"], $inData["id"]);
+		$stmt->execute();	
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	
+?>
