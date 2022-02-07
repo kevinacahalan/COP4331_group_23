080132b772ffcda12cf2abe578ca33c372bfd1f5
Justice Smith <jcode94ttv@gmail.com>
2022-02-07 01:59:42 -0500

    prettier extension formatting

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,7 @@ const urlBase = "https://collectivecontacts.xyz/src/api/";
 const extension = ".php";
 //import { md5 } from './md5.js';
 
-
 async function doLogin() {
-
     const loginForm = document.querySelector("#login");
 
     let login = document.getElementById("username").value;
@@ -13,12 +11,11 @@ async function doLogin() {
 
     let body = {
         login,
-        password: hash
-    }
+        password: hash,
+    };
 
-   // document.getElementById("loginResult").innerHTML = "";
-   setFormMessage(loginForm, "Success", "");
-
+    // document.getElementById("loginResult").innerHTML = "";
+    setFormMessage(loginForm, "Success", "");
 
     let url = urlBase + "Login" + extension;
     let response = await requestHandler(url, body);
@@ -28,13 +25,13 @@ async function doLogin() {
 
     // }
 
-     userId = response.id;
+    userId = response.id;
 
     if (userId < 1) {
         // document.getElementById("loginResult").innerHTML =
-            //   "User/Password combination incorrect";
-         setFormMessage(loginForm, "error", "User/Password combination incorrect");
-         document.getElementById("loginResult").innerHTML = "Incorrect credentials, try again!";
+        //   "User/Password combination incorrect";
+        setFormMessage(loginForm, "error", "User/Password combination incorrect");
+        document.getElementById("loginResult").innerHTML = "Incorrect credentials, try again!";
         return;
     }
 
@@ -47,79 +44,70 @@ async function doLogin() {
 }
 
 async function doSignUp() {
-	userId = 0;
+    userId = 0;
 
-	
-	var firstname = document.getElementById("firstName").value;
-	var lastname = document.getElementById("lastName").value;
-	var login = document.getElementById("signupUsername").value;
+    var firstname = document.getElementById("firstName").value;
+    var lastname = document.getElementById("lastName").value;
+    var login = document.getElementById("signupUsername").value;
 
+    var password = document.getElementById("signupPassword").value;
+    var hash = md5(password);
 
-	var password = document.getElementById("signupPassword").value;
-    var hash = md5( password );
-
-	var confirmPass = document.getElementById("signupConfirmPassword").value;
+    var confirmPass = document.getElementById("signupConfirmPassword").value;
 
     //let formData = formData(document.querySelector("#createAccount"));
 
-	if (password == confirmPass) {
-		document.getElementById("loginResult").innerHTML = "";
+    if (password == confirmPass) {
+        document.getElementById("loginResult").innerHTML = "";
 
-		// "email": "TestEmail@Test.com",
-  		// "password": "password123",
-  		// "firstname": "Landon",
-  		// "lastname": "Russell",
-  		// "phone": "407-938-4910"
+        // "email": "TestEmail@Test.com",
+        // "password": "password123",
+        // "firstname": "Landon",
+        // "lastname": "Russell",
+        // "phone": "407-938-4910"
 
-		var json = { login: login, password: hash };
+        var json = { login: login, password: hash };
 
-		// translating
-		
+        // translating
 
-		// console.log(jsonPayload);
-		
-		var url = urlBase + 'Registration' + extension;
+        // console.log(jsonPayload);
+
+        var url = urlBase + "Registration" + extension;
 
         let body = {
             firstName: firstname,
             lastName: lastname,
             password: hash,
-            login
-        }
+            login,
+        };
 
-		// console.log(url);
+        // console.log(url);
         let response = await requestHandler(url, body);
-        
+
         // TODO: show error for site and stop from auto-redirect
         if (response.error) {
             return;
         }
-        console.log(response)
-		
-        
+        console.log(response);
+
         userId = response.id;
 
         // console.log(userId);
-        
-        localStorage.setItem("userIDInput",userId);
+
+        localStorage.setItem("userIDInput", userId);
 
         localStorage.getItem("userIDInput");
 
         firstName = response.firstName;
         lastName = response.lastName;
 
-
-
         saveCookie();
-        
-        setTimeout(() => window.location.href = "/src/html/contacts.html", 10*1000)
-				
-	}
 
-	else {
-		// make conf pass red
+        setTimeout(() => (window.location.href = "/src/html/contacts.html"), 10 * 1000);
+    } else {
+        // make conf pass red
         document.getElementById("loginResult").innerHTML = "Passwords do not match";
-	}	
+    }
 }
 
 function saveCookie() {
@@ -140,8 +128,8 @@ function saveCookie() {
 // TODO: Think over removal of default values
 async function requestHandler(url = "", formData = {}, rtype = "POST") {
     if (formData === {}) {
-        console.log("Bad input data")
-        return await { error: "No input data" }
+        console.log("Bad input data");
+        return await { error: "No input data" };
     }
 
     let response = await fetch(url, {
@@ -149,18 +137,17 @@ async function requestHandler(url = "", formData = {}, rtype = "POST") {
         headers: {
             "Content-type": "application/json; charset=UTF-8",
         },
-        body: JSON.stringify(formData)
-    })
+        body: JSON.stringify(formData),
+    });
 
     if (!response.ok) {
-        document.getElementById('post-result').innerHTML = `${response.status}`;
+        document.getElementById("post-result").innerHTML = `${response.status}`;
 
         // TODO: Make better error response message
-        return await { error: "Bad response from API"};
+        return await { error: "Bad response from API" };
     }
 
     return await response.json();
-
 }
 
 function setFormMessage(formElement, type, message) {
@@ -181,39 +168,36 @@ function clearInputError(inputElement) {
     inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
 }
 
-function clearButtonResponseText(){
+function clearButtonResponseText() {
     document.getElementById("loginResult_Prime").innerHTML = "";
     document.getElementById("loginResult").innerHTML = "";
 }
 
-function checkFormComplete(){
-    var fnameVal = document.getElementById('firstName').value.length;
-    var lnameVal = document.getElementById('lastName').value.length;
-    var unameVal = document.getElementById('signupUsername').value.length;
-    var pnameVal = document.getElementById('signupPassword').value.length;
-    var cpnameVal = document.getElementById('signupConfirmPassword').value.length;
+function checkFormComplete() {
+    var fnameVal = document.getElementById("firstName").value.length;
+    var lnameVal = document.getElementById("lastName").value.length;
+    var unameVal = document.getElementById("signupUsername").value.length;
+    var pnameVal = document.getElementById("signupPassword").value.length;
+    var cpnameVal = document.getElementById("signupConfirmPassword").value.length;
 
     //If any of the fields are null, return boolean for incomplete TRUE
-     if (fnameVal== "" || lnameVal == "" || unameVal == ""  || pnameVal== "" || cpnameVal==""  ) {
-         //alert("The field needs a value!")
-         return false;
-     }
-     else
-     {
-         return true;
-     }
-
+    if (fnameVal == "" || lnameVal == "" || unameVal == "" || pnameVal == "" || cpnameVal == "") {
+        //alert("The field needs a value!")
+        return false;
+    } else {
+        return true;
+    }
 }
 
-function validate(){
+function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if ( username == "Formget" && password == "formget#123")
-    {
-        alert ("Login successfully");
+    if (username == "Formget" && password == "formget#123") {
+        alert("Login successfully");
         window.location = "../html/contacts.html"; // Redirecting to other page.
         return false;
-    }}
+    }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
@@ -221,7 +205,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const Pass = document.getElementById("signupPassword");
 
     //Create Account Link trigger
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+    document.querySelector("#linkCreateAccount").addEventListener("click", (e) => {
         e.preventDefault();
         loginForm.classList.add("form--hidden");
         clearButtonResponseText();
@@ -229,7 +213,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     //Login Link Trigger
-    document.querySelector("#linkLogin").addEventListener("click", e => {
+    document.querySelector("#linkLogin").addEventListener("click", (e) => {
         e.preventDefault();
         loginForm.classList.remove("form--hidden");
         clearButtonResponseText();
@@ -237,64 +221,57 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     //Controls Create Account Form Button Behavior
-    createAccountForm.addEventListener("submit", e => {
+    createAccountForm.addEventListener("submit", (e) => {
         e.preventDefault();
         //doSignUp();
-       //alert ("Register successfully");
-       // loginForm.classList.remove("form--hidden");
+        //alert ("Register successfully");
+        // loginForm.classList.remove("form--hidden");
         //createAccountForm.classList.add("form--hidden");
 
         //Check if all the inputs have been filled, display message depending on it
-        if( checkFormComplete() == true)
-            {
-              setFormMessage(createAccountForm, "success", "Congrats all fields are filled out!"); 
-              //Now Perform the doSignUp(), but check to see if password matches confirm Passowrd
-              doSignUp(); 
-            }
-        
+        if (checkFormComplete() == true) {
+            setFormMessage(createAccountForm, "success", "Congrats all fields are filled out!");
+            //Now Perform the doSignUp(), but check to see if password matches confirm Passowrd
+            doSignUp();
+        }
+
         //If form isn't complete, don't allow for registration of new user
-        else if(checkFormComplete() == false)
-            {
-                 setFormMessage(createAccountForm, "error", "At least one field is incomplete!");
-            }
-           
-        
+        else if (checkFormComplete() == false) {
+            setFormMessage(createAccountForm, "error", "At least one field is incomplete!");
+        }
 
         //setFormMessage(createAccountForm, "success", "Congrats this test works!");
-
-       
-
     });
 
     //Check login credentials and send via server using doLogin()
-    loginForm.addEventListener("submit", e => {
+    loginForm.addEventListener("submit", (e) => {
         e.preventDefault();
-
-       
 
         doLogin();
         //validate();
-        
     });
 
     //Perform error handling of user input field information
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+    document.querySelectorAll(".form__input").forEach((inputElement) => {
+        inputElement.addEventListener("blur", (e) => {
+            if (
+                e.target.id === "signupUsername" &&
+                e.target.value.length > 0 &&
+                e.target.value.length < 10
+            ) {
                 setInputError(inputElement, "Username must be at least 10 characters in length");
-            }
-
-            else if (e.target.id === "signupPassword" && e.target.value.length > 0 && e.target.value.length < 10) {
+            } else if (
+                e.target.id === "signupPassword" &&
+                e.target.value.length > 0 &&
+                e.target.value.length < 10
+            ) {
                 setInputError(inputElement, "Password must be at least 10 characters in length");
-            }
-
-            else if (e.target.id === "signupConfirmPassword" && Pass.value != e.target.value) {
+            } else if (e.target.id === "signupConfirmPassword" && Pass.value != e.target.value) {
                 setInputError(inputElement, "Confirm password doesn't match password");
             }
-
         });
 
-        inputElement.addEventListener("input", e => {
+        inputElement.addEventListener("input", (e) => {
             clearInputError(inputElement);
         });
     });
