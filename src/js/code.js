204b411ad239496a2ff6f774dc16576f4efdb364
Justice Smith <jcode94ttv@gmail.com>
2022-02-10 13:53:05 -0500

    all modals draggable

--- src/js/code.js
+++ src/js/code.js
@@ -1,13 +1,14 @@
 const urlBase = "https://collectivecontacts.xyz/api";
 const extension = ".php";
 
-let userId = 0;
-let firstName = "";
-let lastName = "";
+let userId = 230;
+let firstName = "Justice";
+let lastName = "Smith";
 
 document.addEventListener(
     "DOMContentLoaded",
     function () {
+        saveCookie();
         readCookie();
 
         document.getElementById("search-submit").addEventListener("click", function (e) {
@@ -30,9 +31,58 @@ document.addEventListener(
         document.getElementById("edit-submit").addEventListener("click", function (e) {
             updateContacts();
         });
+
+        dragElement(document.getElementById("edit-container"));
+        dragElement(document.getElementById("add-container"));
+        dragElement(document.getElementById("search-container"));
+        dragElement(document.getElementById("contacts-container"));
     },
     false
 );
+
+function dragElement(elmnt) {
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    if (document.getElementById(elmnt.id + "-header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "-header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+        elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
 
 function insertContacts(contacts) {
     let contactsList = document.getElementById("contacts-list");
